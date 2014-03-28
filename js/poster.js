d3bac246664dcce6650181ba1309e1d7914d90c8
王同和 <Hetong583@qq.com>
2014-03-28 12:26:11 +0800

    #1

--- js/poster.js
+++ js/poster.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-	// set #container height property
-	{
-		var h = $(document).height()-60;
-		var ph = h.toString();
-		$('#container').css('height', ph);
-		$('#main-info').css('height', ph);
-	};
+	setElementSize();
 });
+$(window).bind('load resize', setElementSize());
+function setElementSize() {
+// set element width and height
+	var h = $(document).height()-60;
+	var posterH = (h / 2),
+			posterW = (h / 6 * 2),
+			sw = (h / 3 * 2);
+	$('#container').css('height', h.toString());
+
+	$('#submit-poster').css('width', sw.toString());
+	$('#submit-container').css('width', (sw + 360).toString());
+
+	$('.posters').css('height', posterH.toString());
+	$('.posters').css('width', posterW.toString());
+	$('.poster-col').css('width', posterW.toString());
+	$('.poster-col').css('height', h.toString());
+};
