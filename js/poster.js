cb0cef27298d9d124d23c97f5683e495dce6b2eb
王同和 <Hetong583@qq.com>
2014-03-31 21:02:16 +0800

    basically complete submit part

--- js/poster.js
+++ js/poster.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
+	var w = $(document).width(),
+			h = $(document).height();
+	var submitWidth = (h-60) / 3 * 2 + 760;
+	var mainInfoContainer = $('#main-info-container');
 	$('#main-info-submit').click(function() {
 		$('#submit-container').slideDown(1000);
-		$('#main-info-container').css('z-index', '-1');
+		if (w<submitWidth)
+			mainInfoContainer.slideUp(400);
 	});
-	setElementSize();
+
+	$('#submit-back-button').click(function() {
+		$('#submit-container').slideUp(1000);
+		if (w<submitWidth)
+			mainInfoContainer.slideDown(1000);
+	});
+
+	function setElementSize() {
+	// set element width and height
+		var containerHeight = h-60;
+		var posterH = (containerHeight / 2),
+				posterW = (containerHeight / 6 * 2),
+				sw = (containerHeight / 3 * 2);
+		$('#container').css('height', containerHeight.toString());
+	
+		$('#submit-poster').css('width', sw.toString());
+		$('#submit-container').css('width', (sw + 360).toString());
+		$('#submit-origin-poster').width(sw / 8 * 3).height(sw / 16 * 9);
+	
+		$('.posters').css('height', posterH.toString()).css('width', posterW.toString());
+		$('.poster-col').css('width', posterW.toString()).css('height', containerHeight.toString());
+	}
+	$(window).resize(setElementSize());
 });
-$(window).bind('load resize', setElementSize());
-function setElementSize() {
-// set element width and height
-	var h = $(document).height()-60;
-	var posterH = (h / 2),
-			posterW = (h / 6 * 2),
-			sw = (h / 3 * 2);
-	$('#container').css('height', h.toString());
-
-	$('#submit-poster').css('width', sw.toString());
-	$('#submit-container').css('width', (sw + 360).toString());
-	$('#submit-origin-poster').width(sw / 8 * 3).height(sw / 16 * 9);
-
-	$('.posters').css('height', posterH.toString()).css('width', posterW.toString());
-	$('.poster-col').css('width', posterW.toString()).css('height', h.toString());
-};
