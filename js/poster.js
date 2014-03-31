f906b82699f9afacd14a2f78e5c360648fd9b646
王同和 <Hetong583@qq.com>
2014-03-31 17:19:01 +0800

    build click part

--- js/poster.js
+++ js/poster.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	$('#main-info-submit').click(function() {
+		$('#submit-container').slideDown(1000);
+		$('#main-info-container').css('z-index', '-1');
+	});
 	setElementSize();
 });
 $(window).bind('load resize', setElementSize());
@@ -14,8 +18,6 @@ function setElementSize() {
 	$('#submit-container').css('width', (sw + 360).toString());
 	$('#submit-origin-poster').width(sw / 8 * 3).height(sw / 16 * 9);
 
-	$('.posters').css('height', posterH.toString());
-	$('.posters').css('width', posterW.toString());
-	$('.poster-col').css('width', posterW.toString());
-	$('.poster-col').css('height', h.toString());
+	$('.posters').css('height', posterH.toString()).css('width', posterW.toString());
+	$('.poster-col').css('width', posterW.toString()).css('height', h.toString());
 };
