a2fac2f987d7a9541362f7437b29fd98bc2de767
王同和 <Hetong583@qq.com>
2014-04-02 09:24:23 +0800

    deep thought

--- js/poster.js
+++ js/poster.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
-	var w = $(document).width(),
-			h = $(document).height();
-	var submitWidth = (h-60) / 3 * 2 + 760;
 	var mainInfoContainer = $('#main-info-container');
 	$('#main-info-submit').click(function() {
+		var w = $(window).width(),
+				h = $(window).height() - 60;
+		var submitWidth = h / 3 * 2 + 760;
 		$('#submit-container').slideDown(1000);
 		if (w<submitWidth)
 			mainInfoContainer.slideUp(400);
 	});
 
 	$('#submit-back-button').click(function() {
+		var w = $(window).width(),
+				h = $(window).height() - 60;
+		var submitWidth = h / 3 * 2 + 760;
 		$('#submit-container').slideUp(1000);
 		if (w<submitWidth)
 			mainInfoContainer.slideDown(1000);
@@ -17,11 +20,15 @@ $(document).ready(function() {
 
 	function setElementSize() {
 	// set element width and height
+		var w = $(window).width(),
+				h = $(window).height();
 		var containerHeight = h-60;
 		var posterH = (containerHeight / 2),
 				posterW = (containerHeight / 6 * 2),
 				sw = (containerHeight / 3 * 2);
 		$('#container').css('height', containerHeight.toString());
+
+		mainInfoContainer.width(sw);
 	
 		$('#submit-poster').css('width', sw.toString());
 		$('#submit-container').css('width', (sw + 360).toString());
@@ -30,5 +37,6 @@ $(document).ready(function() {
 		$('.posters').css('height', posterH.toString()).css('width', posterW.toString());
 		$('.poster-col').css('width', posterW.toString()).css('height', containerHeight.toString());
 	}
-	$(window).resize(setElementSize());
+	setElementSize();
+	window.onresize = setElementSize;
 });
