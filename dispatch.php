870fe9c1e439ce94d823e36417a00e2b47ca6f42
Senorsen <zhs490770@foxmail.com>
2014-04-13 11:12:16 +0800

    add .gitignore

--- dispatch.php
+++ dispatch.php
@@ -22,6 +22,9 @@ class Dispatch {
         $this->db = $db;
         $this->upload_dir = $global_config->upload_dir;
     }
+    public function poster($args) {
+        
+    }
     public function submit_signup($args) {
         $methods = array('online', 'live');
         $requires = array(
