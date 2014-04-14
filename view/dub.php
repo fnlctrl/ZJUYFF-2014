b4d733f52c34067fc65eee6376edf7f8ba4634e8
Senorsen <sen@senorsen.com>
2014-04-14 22:44:53 +0800

    fix bugs

--- view/dub.php
+++ view/dub.php
@@ -15,7 +15,7 @@
         } catch (e) {
             var global_cfg = {"random_token":"error??? pu, we don't know."};
         }
-        var page_cfg = <?php echo json_encode($view_obj->global_cfg);?>;
+        var page_cfg = <?php echo json_encode($view_obj->page_cfg);?>;
     </script>
 	<script src='./js/jquery-2.1.0.min.js'></script>
 	<script src='./js/global.js'></script>
