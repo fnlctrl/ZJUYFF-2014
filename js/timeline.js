3dd1df4bcb0de726fe9cc22f9818709ea3bcfb14
fnlctrl <fnlctrl@gmail.com>
2014-05-07 01:52:53 +0800

    finished timeline

---
+++ js/timeline.js
@@ -0,0 +1,24 @@
+var resize = function() {
+	window.W = $(window).width();
+	window.H = $(window).height();
+	$('.main-event').each(function() {
+		var array = [];
+			$(this).find('.main-text').children('h1, p').each(function() {
+				array.push($(this).width());
+			});
+			window.textWidth = Math.max.apply(null, array);
+		if ($(this).children('img').length>0) {
+			window.imageWidth = window.H * 0.42;
+			$(this).width(window.imageWidth + window.textWidth + 30);
+		}
+		else {
+			$(this).width(window.textWidth);
+		}
+	});
+}
+$(document).ready(function() {
+	setTimeout(resize,50);
+})
+$(window).bind('resize',function() {
+	resize();
+})
--- view/timeline.php
+++ view/timeline.php
@@ -61,97 +61,142 @@
 	</div>
 	<div id='main-container'>
 		<div class='main-day'>
-			<h2>5月4日</h2>
+			<h2 class='main-date'>5月4日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/告诉他们我乘白鹤去了.jpg'/>
-				<p>《告诉他们，<br>我乘白鹤去了》<br>(开幕式)<br>国际会议中心 225<br>18:30-21:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-special'>开幕式</h1><br>
+					<h1 class='main-event-show'>告诉他们，<br>我乘白鹤去了</h1><br>
+					<p>国际会议中心 225<br>18:30-21:00</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月5日</h2>
+			<h2 class='main-date'>5月5日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/算命.jpg'/>
-				<p>《算命》<br>小剧场B座209<br>18:30-21:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>算命</h1><br>
+					<p>小剧场B座209<br>18:30-21:00</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月9日</h2>
+			<h2 class='main-date'>5月9日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/四百击.jpg'/>
-				<p>《四百击》<br>小剧场B座209<br>14:00-17:00</p>
-			</div>
+				<div class='main-text'>
+					<h1 class='main-event-show'>四百击</h1><br>
+					<p>小剧场B座209<br>14:00-17:00</p></div>
+				</div>
 			<div class='main-event'>
 				<img src='./img/timeline/日以作夜.jpg'/>
-				<p>《日以作夜》<br>小剧场B座209<br>18:30-21:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>日以作夜</h1><br>
+					<p>小剧场B座209<br>18:30-21:00</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月10日</h2>
+			<h2 class='main-date'>5月10日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/最后一班地铁.jpg'/>
-				<p>《最后一班地铁》<br>小剧场B座209<br>14:00-17:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>最后一班地铁</h1><br>
+					<p>小剧场B座209<br>14:00-17:00</p>
+				</div>
 			</div>
 			<div class='main-event'>
-				<img src='./img/timeline/最后一班地铁.jpg'/>
-				<p>杭州法语联盟<br>电影分析学者讲座<br>建工之家<br>14:00-17:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-forum' style='margin-left:0'>杭州法语联盟<br>电影分析学者讲座</h1><br>
+					<p>建工之家<br>18:30-21:00</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月11日</h2>
+			<h2 class='main-date'>5月11日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/郎在对门唱山歌.jpg'/>
-				<p>《郎在对门唱山歌》<br>图书馆三楼文化空间<br>13:00-15:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>郎在对门唱山歌</h1><br>
+					<p>图书馆三楼文化空间<br>13:00-15:00</p>
+				</div>
 			</div>
 			<div class='main-event'>
 				<img src='./img/timeline/她们的名字叫红.jpg'/>
-				<p>《她们的名字叫红》<br>图书馆三楼文化空间<br>15:15-17:15</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>她们的名字叫红</h1><br>
+					<p>图书馆三楼文化空间<br>15:15-17:15</p>
+				</div>
 			</div>
 			<div class='main-event'>
-				<img src='./img/timeline/最后一班地铁.jpg'/>
-				<p>北京电影学院教授<br>章明讲座<br>图书馆三楼文化空间<br>19:00-20:30</p>
+				<img src='./img/timeline/章明.jpg'/>
+				<div class='main-text'>
+					<h1 class='main-event-forum'>北京电影学院教授<br>章明讲座</h1><br>
+					<p>图书馆三楼文化空间<br>19:00-20:30</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月16日</h2>
+			<h2 class='main-date'>5月16日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/归途列车.jpg'/>
-				<p>《归途列车》<br>图书馆三楼文化空间<br>15:00-17:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>归途列车</h1><br>
+					<p>图书馆三楼文化空间<br>15:00-17:00</p>
+				</div>
 			</div>
 			<div class='main-event'>
-				<img src='./img/timeline/最后一班地铁.jpg'/>
-				<p>独立纪录片制片人韩轶<br>及影评人卫西谛讲座<br>图书馆三楼文化空间<br>19:00-20:30</p>
+				<img src='./img/timeline/韩轶卫西谛.jpg'/>
+				<div class='main-text'>
+					<h1 class='main-event-special'>独立纪录片制片人韩轶<br>及影评人卫西谛讲座</h1><br>
+					<p>图书馆三楼文化空间<br>19:00-20:30</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月17日</h2>
+			<h2 class='main-date'>5月17日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/千锤百炼.jpg'/>
-				<p>《千锤百炼》<br>图书馆三楼文化空间<br>14:00-16:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>千锤百炼</h1><br>
+					<p>图书馆三楼文化空间<br>14:00-16:00</p>
+				</div>
 			</div>
 			<div class='main-event'>
-				<img src='./img/timeline/最后一班地铁.jpg'/>
-				<p>配音大赛决赛及<br>海报扮演大赛展示和颁奖<br>国际会议中心 223<br>18:30-21:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-special'>配音大赛决赛及<br>海报扮演大赛展示和颁奖<br></h1><br>
+					<p>国际会议中心 223<br>18:30-21:00</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月18日</h2>
+			<h2 class='main-date'>5月18日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/活着.jpg'/>
-				<p>《活着》<br>小剧场B座209<br>18:30-20:30</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>活着</h1><br>
+					<p>小剧场B座209<br>18:30-20:30</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月23日</h2>
+			<h2 class='main-date'>5月23日</h2>
 			<div class='main-event'>
 				<img src='./img/timeline/锤子镰刀都休息.jpg'/>
-				<p>《锤子镰刀都休息》<br>建工之家<br>18:30-21:00</p>
+				<div class='main-text'>
+					<h1 class='main-event-show'>锤子镰刀都休息</h1><br>
+					<p>建工之家<br>18:30-21:00</p>
+				</div>
 			</div>
 		</div>
 		<div class='main-day'>
-			<h2>5月24日</h2>
+			<h2 class='main-date'>5月24日</h2>
 			<div class='main-event'>
-				<img src='./img/timeline/最后一班地铁.jpg'/>
-				<p>闭幕式暨<br>微电影展映和高校论坛<br>图书馆三楼文化空间<br>18:30-21:30</p>
+				<div class='main-text'>
+					<h1 class='main-event-special'>闭幕式暨<br>微电影展映和高校论坛</h1><br>
+					<p>图书馆三楼文化空间<br>18:30-21:30</p>
+				</div>
 			</div>
 		</div>
 	</div>
